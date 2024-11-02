e497c05568459991e5f5c5fd8d6027084561668f
Alexander DeJesus <alexdejesus336@Yahoo.com>
2024-11-02 14:55:46 -0400

    Created user model

---
+++ Backend/src/models/userModel.js
@@ -0,0 +1,12 @@
+// src/models/userModel.js
+class User {
+    constructor(id, name, email, password) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.password = password;
+    }
+  }
+  
+  module.exports = User;
+  
